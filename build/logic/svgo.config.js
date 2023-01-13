efdd4ff6cbcf004ab4d3c54c450e79d210697f0a
Christian Oliff <christianoliff@pm.me>
2023-01-13 21:23:50 +0900

    Add more icons metadata (#27)

--- build/logic/svgo.config.js
+++ build/logic/svgo.config.js
@@ -18,6 +18,7 @@ module.exports = {
             keepRoleAttr: true,
           },
           removeViewBox: false,
+          sortAttrs: true,
         },
       },
     },
---
+++ docs/content-pages/stats.md
@@ -0,0 +1,10 @@
+---
+title: "Stats"
+layout: "stats"
+url: "/stats/"
+hidden: true
+private: true
+---
+
+<h1 class="display-1 text-center mx-auto py-3 mt-5">Stats</h1>
+

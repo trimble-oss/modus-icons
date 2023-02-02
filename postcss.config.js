7cabe3b7beb9581f41c257a98ae14d8c002def36
Christian Oliff <christianoliff@pm.me>
2023-02-02 20:48:50 +0900

    Update Templates and CSS optimizations

--- postcss.config.js
+++ postcss.config.js
@@ -179,6 +179,7 @@ module.exports = {
         'sun',
         'table',
         'table-bordered',
+        'text-capitalize',
         'text-center',
         'text-danger',
         'text-decoration-none',
